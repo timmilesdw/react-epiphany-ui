1d72925e4a415c40ff7df812b94121ee6bb4b5a5
tim <bulletcries13@gmail.com>
2020-05-11 01:03:45 +0500

    improved example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, ETheme } from 'react-epiphany-ui'
 
 
 const App = () => {
+  const [color, setColor] = useState()
+  const [size, setSize] = useState('default')
+  const [event, setEvent] = useState(false)
   return(
     <ETheme>
-      <div style={{display: 'flex', justifyContent: 'center', marginTop: '10%'}}>
-        <Button onClick={() => console.log('shish')} label="My Button"/>
+      <div style={{display: 'flex', justifyContent: 'space-around', marginTop: '10%'}}>
+        <div>
+          <div>Color</div>
+          <input 
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="default"/>
+        </div>
+        <div>
+          <div>Size</div>
+          <input 
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            placeholder="xs, sm, md or lg"/>
+        </div>
+        <div>
+          <div>Event</div>
+          <div>{event ? 'true' : 'false'}</div>
+        </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '2%'}}>
+        <Button color={color} size={size} onClick={() => setEvent(!event)} label="press me"/>
       </div>
     </ETheme>
   )
